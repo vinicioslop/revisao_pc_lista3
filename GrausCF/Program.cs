6e7ba8606b1a3844e7a409f8f87f8891bf84518a
vinicioslop <vinicioslop7@hotmail.com>
2021-09-17 20:44:43 -0300

    Finaliza a conversao de celsius para f....

---
+++ GrausCF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Double celsius, fahr;
+
+            Console.WriteLine("Bem vindo ao conversor de graus C° em F°!");
+
+            Console.Write("\nDigite o valor em graus celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahr = celsius * 1.8 + 32;
+
+            Console.WriteLine($"\n{celsius}° equivale a {fahr}°F");
+        }
+    }
+}
