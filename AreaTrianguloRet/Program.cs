39b2f4c1938a56b3d135d9b6b35fc3945ef6e189
vinicioslop <vinicioslop7@hotmail.com>
2021-09-17 20:37:29 -0300

    Finaliza a atividade de calculo da área do triangulo retangulo

---
+++ AreaTrianguloRet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AreaTrianTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Double baseTrian, alturaTrian, areaTrian;
+
+            Console.WriteLine("Bem vindo ao calculo de area de triangulo retangulo");
+
+            Console.WriteLine();
+
+            Console.Write("Digite o valor da base..: ");
+            baseTrian = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura: ");
+            alturaTrian = Convert.ToDouble(Console.ReadLine());
+
+            areaTrian = (baseTrian * alturaTrian) / 2;
+
+            Console.WriteLine($"\nBase: {baseTrian}. Altura: {alturaTrian}");
+            Console.WriteLine($"Área: {areaTrian.ToString("N1")}");
+        }
+    }
+}
