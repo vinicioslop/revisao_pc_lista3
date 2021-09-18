72f6bd81f6b19aa9ff9d7000ee148fd6eeaaeeec
vinicioslop <vinicioslop7@hotmail.com>
2021-09-17 21:06:29 -0300

    Finaliza o calculo de Juros Simples

---
+++ JurosSimples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcule o montante final de um investimento a juros simples.
+            //j = c . i . t
+            //m = c + j
+
+            Console.Clear();
+
+            Decimal montante, tempo, taxaJuros, capital, juros;
+
+            Console.WriteLine("Bem vindo a calculadora de Juros Simples!");
+
+            Console.Write("\nCapital [c] (R$).......: ");
+            capital = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Taxa de juros [i] (%)..: ");
+            taxaJuros = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Tempo [t] (meses)......: ");
+            tempo = Convert.ToDecimal(Console.ReadLine());
+
+            juros = capital * (taxaJuros / 100) * tempo;
+            montante = capital + juros;
+
+            Console.WriteLine($"\nJuros (R$).....: {juros.ToString("C")}");
+            Console.WriteLine($"Montante (R$)..: {montante.ToString("C")}");
+        }
+    }
+}
